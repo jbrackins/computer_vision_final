bc60f9663849d8570e9c919ff0200c3d3aff02e0
Julian Brackins <julian.brackins@mines.sdsmt.edu>
2015-12-11 15:10:01 -0700

    setting up repo

---
+++ Distortion.m
@@ -0,0 +1,13 @@
+function Distortion( image, aplitude, type )
+%Distortion warp image to appear as if they have been wrapped around sphere
+%%% Julian Anthony Brackins   %%%
+%%% CSC 514 - Computer Vision %%%
+%%% Final Exam                %%%
+    
+    clc
+    close all
+
+    if nargin < 3
+       type = 'BARREl'; 
+    end
+end
---
+++ FindWindows.m
@@ -0,0 +1,12 @@
+function FindWindows( image )
+%FindWindows Display Image with red rectangluar boxes outlining each window
+%%% Julian Anthony Brackins   %%%
+%%% CSC 514 - Computer Vision %%%
+%%% Final Exam                %%%
+
+    clc
+    close all
+
+
+end
+
